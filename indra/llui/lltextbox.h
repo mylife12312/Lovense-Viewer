c72f91fd0dbeb6cd3434d12e6bc0f175ea03c0ec
cita <754759490@qq.com>
2024-05-31 14:32:14 +0800

    fixed build error

--- indra/llui/lltextbox.h
+++ indra/llui/lltextbox.h
@@ -68,6 +68,9 @@ public:
     /*virtual*/ LLSD    getValue() const;
     /*virtual*/ BOOL    setTextArg( const std::string& key, const LLStringExplicit& text );
 
+    void setShowCursorHand(bool show_cursor) { mShowCursorHand = show_cursor; }
+
+
 	bool GetNotLinkUrlAction() const { return mNotLinkUrlAction; }
     void SetNotLinkUrlAction(bool link) { mNotLinkUrlAction = link; }
 
